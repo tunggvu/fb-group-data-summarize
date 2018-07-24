463435c48b195478469183e5bc518b867ef1bcbf
Chu Hoang Nam <39687218+chuhoangnam@users.noreply.github.com>
2018-07-24 15:36:30 +0700

    [BE][Hotfix] Fix upgrade-front script typo (#39)

--- upgrade-front.sh
+++ upgrade-front.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
-docker exec $(docker container ls -q -f NAME=haproxy) haproxy_set_server down emres-server/front-1
-docker exec $(docker container ls -q -f NAME=haproxy) haproxy_set_server down emres-server/front-2
+docker exec $(docker container ls -q -f NAME=haproxy) haproxy_set_server down emres-front/front-1
+docker exec $(docker container ls -q -f NAME=haproxy) haproxy_set_server down emres-front/front-2
 docker-compose up -d front-1
 docker-compose up -d front-2
-docker exec $(docker container ls -q -f NAME=haproxy) haproxy_set_server up emres-server/front-1
-docker exec $(docker container ls -q -f NAME=haproxy) haproxy_set_server up emres-server/front-2
+docker exec $(docker container ls -q -f NAME=haproxy) haproxy_set_server up emres-front/front-1
+docker exec $(docker container ls -q -f NAME=haproxy) haproxy_set_server up emres-front/front-2
 
-docker exec $(docker container ls -q -f NAME=haproxy) haproxy_set_server down emres-server/front-3
-docker exec $(docker container ls -q -f NAME=haproxy) haproxy_set_server down emres-server/front-4
+docker exec $(docker container ls -q -f NAME=haproxy) haproxy_set_server down emres-front/front-3
+docker exec $(docker container ls -q -f NAME=haproxy) haproxy_set_server down emres-front/front-4
 docker-compose up -d front-3
 docker-compose up -d front-4
-docker exec $(docker container ls -q -f NAME=haproxy) haproxy_set_server up emres-server/front-3
-docker exec $(docker container ls -q -f NAME=haproxy) haproxy_set_server up emres-server/front-4
+docker exec $(docker container ls -q -f NAME=haproxy) haproxy_set_server up emres-front/front-3
+docker exec $(docker container ls -q -f NAME=haproxy) haproxy_set_server up emres-front/front-4
 
 # Make HAproxy reload the config file
 # docker kill -s HUP emres-server_haproxy_1

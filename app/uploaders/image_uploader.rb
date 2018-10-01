0e319adb25d70742a04d957c3e729670c0cbff8d
Nguyễn Văn Sang <nguyen.van.sang@framgia.com>
2018-10-01 16:07:34 +0700

    Config unique image file name to avoid server cache (#136)

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -3,6 +3,10 @@
 class ImageUploader < CarrierWave::Uploader::Base
   storage :file
 
+  def filename
+    "#{secure_token}.#{file.extension}" if original_filename.present?
+  end
+
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
@@ -13,6 +17,13 @@ class ImageUploader < CarrierWave::Uploader::Base
 
   def default_url
     "/uploads/avatar.png"
+  end
+
+  protected
+
+  def secure_token
+    var = :"@#{mounted_as}_secure_token"
+    model.instance_variable_get(var) || model.instance_variable_set(var, SecureRandom.uuid)
   end
   # TODO config image upload
 end

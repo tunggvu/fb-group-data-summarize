1ebb7ed141fb0b7cec5567e8818160acc18c615f
Trịnh Đình Việt <32216811+tdvietdev@users.noreply.github.com>
2018-10-17 16:05:49 +0700

    fix request seed (#149)
    [BE][Task][421] Fix seed for request

--- db/fixtures/0110_request.rb
+++ db/fixtures/0110_request.rb
@@ -1,19 +1,21 @@
 # frozen_string_literal: true
 
 MAX_TIME = 5
-MAX_DAY_AGO = 6
-statuses = Request.statuses.slice(:approved, :rejected)
+events = [:approve!, :confirm!, :reject!]
 
 Device.all.each do |device|
   MAX_TIME.times.each do |n|
     project = Project.all.sample
-    Request.seed do |r|
-      r.device = device
-      r.project = project
-      r.request_pic = project.employees.sample
-      r.requester = project.product_owner
-      r.modified_date = (MAX_DAY_AGO - n).days.ago
-      r.status = n == MAX_TIME ? Request.statuses[:pending] : statuses[statuses.keys.sample]
-    end
+    request = Request.create!(
+      device: device,
+      project: project,
+      request_pic: project.employees.sample,
+      requester: project.product_owner,
+      modified_date: n.days.from_now
+    )
+    confirmation_token = Request.new_token
+    confirmation_digest = Request.digest confirmation_token
+    request.update_column(:confirmation_digest, confirmation_digest)
+    request.send(events.sample, confirmation_token)
   end
 end
